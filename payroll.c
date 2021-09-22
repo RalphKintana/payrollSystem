e738e32f9ee0b2004ce980f3cd9d7ee84ed5db5c
aeolius315 <54928707+aeolius315@users.noreply.github.com>
2021-09-22 10:28:04 +0800

    Added C file

---
+++ payroll.c
@@ -0,0 +1,20 @@
+/* ADMIN 
+1. Time-In
+2. Time-Out
+3. Display Employee
+4. Generate Payroll Report
+5. Update Employee
+6. Delete Employee
+7. Calculate my salary income
+8. exit
+*/
+
+/* EMPLOYEE
+1. Time-In
+2. Time-Out
+3. Show rendered hours
+4. Calculate my salary income
+5. Exit
+*/
+
+
